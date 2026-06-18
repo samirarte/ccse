c938324b6f07911f2a2755b04d091799d2d0aecd
roberto.bergua <roberto.bergua@gmail.com>
2026-06-18 14:20:38 +0000

    1

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyALcDySqvhl6eGAwM4B95crBM_keRKk48U",
   authDomain: "ccse-96981979-90250.firebaseapp.com",
+  databaseURL: "https://ccse-96981979-90250.firebaseio.com",
   projectId: "ccse-96981979-90250",
   storageBucket: "ccse-96981979-90250.firebasestorage.app",
   messagingSenderId: "18998504232",
